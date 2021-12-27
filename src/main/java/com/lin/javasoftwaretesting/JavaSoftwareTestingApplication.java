d2be1b9a2a73658fbf9e85f3c4389eeabfc42c6c
MD HASAN <hasan.mmd28@gmail.com>
2021-12-27 15:25:37 -0500

    first commit

---
+++ src/main/java/com/lin/javasoftwaretesting/JavaSoftwareTestingApplication.java
@@ -0,0 +1,13 @@
+package com.lin.javasoftwaretesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSoftwareTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSoftwareTestingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lin/javasoftwaretesting/JavaSoftwareTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lin.javasoftwaretesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSoftwareTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
